599d3fa8d2f976b385e7545515e18e8105a965ba
Synapticloop <osmanj@15-Inches-of-Fun-2.local>
2016-03-08 23:41:22 +0000

    added help* method signatures, documentr

--- README.md
+++ README.md
@@ -1,3 +1,228 @@
-# Simpleusage
+[![Build Status](https://travis-ci.org/synapticloop/simpleusage.svg?branch=master)](https://travis-ci.org/synapticloop/simpleusage) [![Download](https://api.bintray.com/packages/synapticloop/maven/simpleusage/images/download.svg)](https://bintray.com/synapticloop/maven/simpleusage/_latestVersion) [![GitHub Release](https://img.shields.io/github/release/synapticloop/simpleusage.svg)](https://github.com/synapticloop/simpleusage/releases) 
 
-[![Build Status](https://travis-ci.org/synapticloop/simpleusage.svg?branch=master)](https://travis-ci.org/synapticloop/simpleusage)
+# simpleusage
+
+
+
+> a very simple usage message generator
+
+
+
+# Usage
+
+Sometimes, you just need to print out a simple usage message, this will look in the classpath to find the text files and output them to the console.
+
+There are two usage scenarios:
+
+ 1. Print out a simple usage message
+ 1. Print out a longer, more helpful usage message
+
+If there is a `Throwable` parameter, the stacktrace will be printed to the console
+
+The `usage` method signatures will look in the classpath for a file named `USAGE.txt` or `usage.txt` and output the contents (if found) to the console.
+
+The `help` method signatures will look in the classpath for a file named `HELP.txt` or `help.txt`, this will first output the `USAGE.txt` file, and then the `HELP.txt` file.
+
+## Method signatures
+
+
+```
+	// print out the usage message
+	public static void usage()
+	public static void usage(Throwable throwable)
+	public static void usage(String message)
+	public static void usage(String message, Throwable throwable)
+
+	// print out the usage message and exit
+	public static void usageAndExit()
+	public static void usageAndExit(Throwable throwable)
+	public static void usageAndExit(String message)
+	public static void usageAndExit(String message, Throwable throwable)
+	public static void usageAndExit(int exitCode)
+	public static void usageAndExit(int exitCode, Throwable throwable)
+	public static void usageAndExit(String message, int exitCode)
+	public static void usageAndExit(String message, int exitCode, Throwable throwable)
+
+	// print out the usage message, followed by the longer help message
+	public static void help()
+	public static void help(Throwable throwable)
+	public static void help(String message)
+	public static void help(String message, Throwable throwable)
+
+	// print out the usage message, followed by the longer help message and exit
+	public static void helpAndExit()
+	public static void helpAndExit(Throwable throwable)
+	public static void helpAndExit(String message)
+	public static void helpAndExit(String message, Throwable throwable)
+	public static void helpAndExit(int exitCode)
+	public static void helpAndExit(int exitCode, Throwable throwable)
+	public static void helpAndExit(String message, int exitCode)
+`\t}public static void helpAndExit(String message, int exitCode, Throwable throwable)
+```
+
+
+All message parameters will be pre-pended by  ` [ FATAL ] ` designator.
+
+# Building the Package
+
+## *NIX/Mac OS X
+
+From the root of the project, simply run
+
+`./gradlew build`
+
+
+## Windows
+
+`./gradlew.bat build`
+
+
+This will compile and assemble the artefacts into the `build/libs/` directory.
+
+Note that this may also run tests (if applicable see the Testing notes)
+
+# Artefact Publishing - Github
+
+This project publishes artefacts to [GitHib](https://github.com/)
+
+> Note that the latest version can be found [https://github.com/synapticloop/simpleusage/releases](https://github.com/synapticloop/simpleusage/releases)
+
+As such, this is not a repository, but a location to download files from.
+
+# Artefact Publishing - Bintray
+
+This project publishes artefacts to [bintray](https://bintray.com/)
+
+> Note that the latest version can be found [https://bintray.com/synapticloop/maven/simpleusage/view](https://bintray.com/synapticloop/maven/simpleusage/view)
+
+## maven setup
+
+this comes from the jcenter bintray, to set up your repository:
+
+```
+<?xml version="1.0" encoding="UTF-8" ?>
+<settings xsi:schemaLocation='http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd' xmlns='http://maven.apache.org/SETTINGS/1.0.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'>
+  <profiles>
+    <profile>
+      <repositories>
+        <repository>
+          <snapshots>
+            <enabled>false</enabled>
+          </snapshots>
+          <id>central</id>
+          <name>bintray</name>
+          <url>http://jcenter.bintray.com</url>
+        </repository>
+      </repositories>
+      <pluginRepositories>
+        <pluginRepository>
+          <snapshots>
+            <enabled>false</enabled>
+          </snapshots>
+          <id>central</id>
+          <name>bintray-plugins</name>
+          <url>http://jcenter.bintray.com</url>
+        </pluginRepository>
+      </pluginRepositories>
+      <id>bintray</id>
+    </profile>
+  </profiles>
+  <activeProfiles>
+    <activeProfile>bintray</activeProfile>
+  </activeProfiles>
+</settings>
+```
+
+## gradle setup
+
+Repository
+
+```
+repositories {
+	maven {
+		url  "http://jcenter.bintray.com" 
+	}
+}
+```
+
+or just
+
+```
+repositories {
+	jcenter()
+}
+```
+
+## Dependencies - Gradle
+
+```
+dependencies {
+	runtime(group: 'synapticloop', name: 'simpleusage', version: 'v1.1.0', ext: 'jar')
+
+	compile(group: 'synapticloop', name: 'simpleusage', version: 'v1.1.0', ext: 'jar')
+}
+```
+
+or, more simply for versions of gradle greater than 2.4
+
+```
+dependencies {
+	runtime 'synapticloop:simpleusage:v1.1.0'
+
+	compile 'synapticloop:simpleusage:v1.1.0'
+}
+```
+
+## Dependencies - Maven
+
+```
+<dependency>
+	<groupId>synapticloop</groupId>
+	<artifactId>simpleusage</artifactId>
+	<version>v1.1.0</version>
+	<type>jar</type>
+</dependency>
+```
+
+## Other packages
+
+
+
+You will also need the dependencies:
+
+
+**NOTE:** You may need to download any dependencies of the above dependencies in turn
+
+# License
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2016 synapticloop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+
+--
+
+> `This README.md file was hand-crafted with care utilising synapticloop`[`templar`](https://github.com/synapticloop/templar/)`->`[`documentr`](https://github.com/synapticloop/documentr/)
+
+--
+
--- src/main/java/synapticloop/util/SimpleUsage.java
+++ src/main/java/synapticloop/util/SimpleUsage.java
@@ -6,10 +6,42 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class SimpleUsage {
-	private static final String USAGE_TXT_LOWERCASE = "/usage.txt";
 	private static final String USAGE_TXT_UPPERCASE = "/USAGE.txt";
+	private static final String HELP_TXT_UPPERCASE = "/HELP.txt";
 
-	private static final String FATAL_COULD_NOT_FIND_USAGE = "[ FATAL ] Could not find the 'usage.txt' or 'USAGE.txt' on the classpath.\n  No instructions can be found.";
+	private static final String FATAL_COULD_NOT_FIND_USAGE = " [ FATAL ] Could not find the 'usage.txt' or 'USAGE.txt' on the classpath.\n  No instructions can be found.";
+	private static final String FATAL_COULD_NOT_FIND_HELP = " [ FATAL ] Could not find the 'help.txt' or 'HELP.txt' on the classpath.\n  No instructions can be found.";
+
+	/**
+	 * Print out a message and then the usage instructions to the console.  If 
+	 * the message is null, then it will not be printed.  This will look in the
+	 * classpath for either a usage.txt or a USAGE.txt and print out the 
+	 * contents.  If neither can be found, then a FATAL message will be printed
+	 * to the console.
+	 * 
+	 * @param message The specific message to print to the console, or nothing if
+	 *     null
+	 * @param throwable A Throwable, which, if not null will will print out the 
+	 *     stacktrace
+	 */
+	public static void usage(String message, Throwable throwable) {
+		if(null != message) {
+			System.out.println(String.format(" [ FATAL ] message was: ", message));
+		}
+
+		if(null != throwable) {
+			System.out.println("\n");
+			throwable.printStackTrace();
+			System.out.println("\n");
+		}
+
+		InputStream inputStream = SimpleUsage.class.getResourceAsStream(USAGE_TXT_UPPERCASE);
+		if(null == inputStream) {
+			inputStream = SimpleUsage.class.getResourceAsStream(USAGE_TXT_UPPERCASE.toLowerCase());
+		}
+
+		outputFile(inputStream, FATAL_COULD_NOT_FIND_USAGE);
+	}
 
 	/**
 	 * Print out a message and then the usage instructions to the console.  If 
@@ -22,15 +54,208 @@ public class SimpleUsage {
 	 * null
 	 */
 	public static void usage(String message) {
+		usage(message, null);
+	}
+
+
+	/**
+	 * Print out the instructional usage message.
+	 */
+	public static void usage() {
+		usage(null, null);
+	}
+
+	public static void usage(Throwable throwable) {
+		usage(null, throwable);
+	}
+
+	/**
+	 * Print out the instructional usage message and System.exit with an exit 
+	 * code of -1
+	 */
+
+	public static void usageAndExit() {
+		usage(null, null);
+		System.exit(-1);
+	}
+
+	public static void usageAndExit(Throwable throwable) {
+		usage(null, throwable);
+		System.exit(-1);
+	}
+
+	/**
+	 * Print out the instructional usage message and System.exit with an exit 
+	 * code of -1
+	 * 
+	 * @param message the message to print before the usage.txt
+	 */
+	public static void usageAndExit(String message) {
+		usage(message, null);
+		System.exit(-1);
+	}
+
+	public static void usageAndExit(String message, Throwable throwable) {
+		usage(message, throwable);
+		System.exit(-1);
+	}
+
+	/**
+	 * Print out the instructional usage message and System.exit with the 
+	 * provided exit code.
+	 * 
+	 * @param exitCode The exit code to System.exit with
+	 */
+	public static void usageAndExit(int exitCode) {
+		usage(null, null);
+		System.exit(exitCode);
+	}
+
+	public static void usageAndExit(int exitCode, Throwable throwable) {
+		usage(null, throwable);
+		System.exit(exitCode);
+	}
+
+	/**
+	 * Print out the passed in message and then the instructional usage message 
+	 * and System.exit with the provided exit code.
+	 * 
+	 * @param message the message to print before the usage.txt
+	 * @param exitCode The exit code to System.exit with
+	 */
+	public static void usageAndExit(String message, int exitCode) {
+		usage(message, null);
+		System.exit(exitCode);
+	}
+
+	public static void usageAndExit(String message, int exitCode, Throwable throwable) {
+		usage(message, throwable);
+		System.exit(exitCode);
+	}
+
+	/**
+	 * Print out a message and then the help instructions to the console.  If 
+	 * the message is null, then it will not be printed.  This will look in the
+	 * classpath for either a help.txt or a USAGE.txt and print out the 
+	 * contents.  If neither can be found, then a FATAL message will be printed
+	 * to the console.
+	 * 
+	 * @param message The specific message to print to the console, or nothing if
+	 *     null
+	 * @param throwable A Throwable, which, if not null will will print out the 
+	 *     stacktrace
+	 */
+	public static void help(String message, Throwable throwable) {
 		if(null != message) {
-			System.out.println(message);
+			System.out.println(String.format(" [ FATAL ] message was: ", message));
 		}
 
-		InputStream inputStream = SimpleUsage.class.getResourceAsStream(USAGE_TXT_LOWERCASE);
+		if(null != throwable) {
+			System.out.println("\n");
+			throwable.printStackTrace();
+			System.out.println("\n");
+		}
+
+		InputStream inputStream = SimpleUsage.class.getResourceAsStream(HELP_TXT_UPPERCASE);
 		if(null == inputStream) {
-			inputStream = SimpleUsage.class.getResourceAsStream(USAGE_TXT_UPPERCASE);
+			inputStream = SimpleUsage.class.getResourceAsStream(HELP_TXT_UPPERCASE.toLowerCase());
 		}
 
+		outputFile(inputStream, FATAL_COULD_NOT_FIND_USAGE);
+		outputFile(inputStream, FATAL_COULD_NOT_FIND_HELP);
+	}
+
+	/**
+	 * Print out a message and then the help instructions to the console.  If 
+	 * the message is null, then it will not be printed.  This will look in the
+	 * classpath for either a help.txt or a USAGE.txt and print out the 
+	 * contents.  If neither can be found, then a FATAL message will be printed
+	 * to the console.
+	 * 
+	 * @param message The specific message to print to the console, or nothing if
+	 * null
+	 */
+	public static void help(String message) {
+		help(message, null);
+	}
+
+
+	/**
+	 * Print out the instructional help message.
+	 */
+	public static void help() {
+		help(null, null);
+	}
+
+	public static void help(Throwable throwable) {
+		help(null, throwable);
+	}
+
+	/**
+	 * Print out the instructional help message and System.exit with an exit 
+	 * code of -1
+	 */
+
+	public static void helpAndExit() {
+		help(null, null);
+		System.exit(-1);
+	}
+
+	public static void helpAndExit(Throwable throwable) {
+		help(null, throwable);
+		System.exit(-1);
+	}
+
+	/**
+	 * Print out the instructional help message and System.exit with an exit 
+	 * code of -1
+	 * 
+	 * @param message the message to print before the help.txt
+	 */
+	public static void helpAndExit(String message) {
+		help(message, null);
+		System.exit(-1);
+	}
+
+	public static void helpAndExit(String message, Throwable throwable) {
+		help(message, throwable);
+		System.exit(-1);
+	}
+
+	/**
+	 * Print out the instructional help message and System.exit with the 
+	 * provided exit code.
+	 * 
+	 * @param exitCode The exit code to System.exit with
+	 */
+	public static void helpAndExit(int exitCode) {
+		help(null, null);
+		System.exit(exitCode);
+	}
+
+	public static void helpAndExit(int exitCode, Throwable throwable) {
+		help(null, throwable);
+		System.exit(exitCode);
+	}
+
+	/**
+	 * Print out the passed in message and then the instructional help message 
+	 * and System.exit with the provided exit code.
+	 * 
+	 * @param message the message to print before the help.txt
+	 * @param exitCode The exit code to System.exit with
+	 */
+	public static void helpAndExit(String message, int exitCode) {
+		help(message, null);
+		System.exit(exitCode);
+	}
+
+	public static void helpAndExit(String message, int exitCode, Throwable throwable) {
+		help(message, throwable);
+		System.exit(exitCode);
+	}
+
+	private static void outputFile(InputStream inputStream, String errorMessage) {
 		BufferedReader bufferedReader = null;
 
 		if(null != inputStream) {
@@ -51,57 +276,6 @@ public class SimpleUsage {
 			// input stream is still null 
 			System.out.println(FATAL_COULD_NOT_FIND_USAGE);
 		}
-	}
-
-	/**
-	 * Print out the instructional usage message.
-	 */
-	public static void usage() {
-		usage(null);
-	}
-
-	/**
-	 * Print out the instructional usage message and System.exit with an exit 
-	 * code of -1
-	 */
-	
-	public static void usageAndExit() {
-		usage(null);
-		System.exit(-1);
-	}
-
-	/**
-	 * Print out the instructional usage message and System.exit with an exit 
-	 * code of -1
-	 * 
-	 * @param message the message to print before the usage.txt
-	 */
-	public static void usageAndExit(String message) {
-		usage(message);
-		System.exit(-1);
-	}
-
-	/**
-	 * Print out the instructional usage message and System.exit with the 
-	 * provided exit code.
-	 * 
-	 * @param exitCode The exit code to System.exit with
-	 */
-	public static void usageAndExit(int exitCode) {
-		usage(null);
-		System.exit(exitCode);
-	}
-
-	/**
-	 * Print out the passed in message and then the instructional usage message 
-	 * and System.exit with the provided exit code.
-	 * 
-	 * @param message the message to print before the usage.txt
-	 * @param exitCode The exit code to System.exit with
-	 */
-	public static void usageAndExit(String message, int exitCode) {
-		usage(message);
-		System.exit(exitCode);
 	}
 
 }
